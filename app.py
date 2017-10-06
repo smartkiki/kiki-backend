f309c82f4b5d3e81809d2d1bfb0e6aaee12b1666
Eehita Parameswaran <eehitap@gmail.com>
2017-10-05 20:18:38 -0400

    stt working

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import sys, json, requests
 from flask import Flask, request
 from nlp import response as nlp
+from stt import speech_recognizer as stt
 import os
 
-
-
 app = Flask(__name__)
-
-
 
 def message_handler():
 		"""
@@ -15,18 +12,18 @@ def message_handler():
 		"""
 
 		# get the user's input and store in user_input variable.
-		user_input = "What's up?"
-
-		"""
-		TEXT PROCESSING CODE
-		Parses text and returns response string.
-		"""
-		nlp_handler = nlp.Response()
-		responseText = nlp_handler.get_response(user_input)
-		
-		#Removing single quotes in output string and converting text to speech
-		output = responseText.translate(str.maketrans({"'":None}))
-		os.system("say '%s'" % output)
+		while True:
+			user_input = stt.wait_for_input()
+			"""
+			TEXT PROCESSING CODE
+			Parses text and returns response string.
+			"""
+			nlp_handler = nlp.Response()
+			responseText = nlp_handler.get_response(user_input)
+			
+			#Removing single quotes in output string and converting text to speech
+			output = responseText #.translate(str) #.maketrans({"'":None}))
+			os.system("say '%s'" % output)
 
 
 
